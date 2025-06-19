1132af9eb567f3455b1c77ad9ecb52853903ba12
asifali45 <asifali4556nagaroor@gmail.com>
2025-06-19 20:49:23 +0530

    Add project files.

---
+++ EmployeePortalSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EmployeePortalSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
