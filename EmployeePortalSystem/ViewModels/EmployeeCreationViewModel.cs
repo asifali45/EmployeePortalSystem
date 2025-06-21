f3c34c28989ae355cfd9e1311bad0c3ef47640fa
amarthyalekshmi <145977423+amarthyalekshmi@users.noreply.github.com>
2025-06-21 20:55:18 +0530

    Employee insertion

---
+++ EmployeePortalSystem/ViewModels/EmployeeCreationViewModel.cs
@@ -0,0 +1,14 @@
+﻿using EmployeePortalSystem.Models;
+using System.Collections.Generic;
+
+namespace EmployeePortalSystem.ViewModels
+{
+    public class EmployeeCreationViewModel
+    {
+        public Employee Employee { get; set; }
+
+        public List<Department> Departments { get; set; }
+
+        public List<Role> Roles { get; set; }
+    }
+}
