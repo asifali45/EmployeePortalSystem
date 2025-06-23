b3ed154e7742d09eab268a2da2df7f2b1d9eceaa
Fahana <Fahana@LAPTOP-S7V9PGL4>
2025-06-23 13:38:52 +0530

    created department page with add ,edit ,delete options

--- EmployeePortalSystem/Controllers/DepartmentsController.cs
+++ EmployeePortalSystem/Controllers/DepartmentsController.cs
@@ -1,12 +1,63 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EmployeePortalSystem.Models;
+using Microsoft.AspNetCore.Mvc;
+using EmployeePortalSystem.Repositories;
+
 
 namespace EmployeePortalSystem.Controllers
 {
     public class DepartmentsController : Controller
     {
+        private readonly IDepartmentRepository _repo;
+
+        public DepartmentsController(IDepartmentRepository repo)
+        {
+            _repo = repo;
+        }
+
         public IActionResult Index()
         {
-            return View();
+
+            var departments = _repo.GetAll();
+            return View(departments);
         }
+
+        public IActionResult Details(int id)
+        {
+            var dept = _repo.GetById(id);
+            return View(dept);
+        }
+
+        public IActionResult Create() => View();
+
+        [HttpPost]
+        public IActionResult Create(Department dept)
+        {
+            dept.CreatedBy = 1;
+            _repo.Add(dept);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var dept = _repo.GetById(id);
+            return View(dept);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Department dept)
+        {
+            dept.UpdatedBy = 1;
+            _repo.Update(dept);
+            return RedirectToAction("Index");
+        }
+
+
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            _repo.Delete(id);
+            return RedirectToAction("Index");
+        }
+
     }
 }
---
+++ EmployeePortalSystem/Repositories/DepartmentRepository.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using EmployeePortalSystem.Models;
+using System.Data;
+using Dapper;
+using MySql.Data.MySqlClient;
+
+namespace EmployeePortalSystem.Repositories
+{
+
+
+    public class DepartmentRepository : IDepartmentRepository
+    {
+        private readonly string _connectionString;
+
+        public DepartmentRepository(IConfiguration config)
+        {
+            _connectionString = config.GetConnectionString("DefaultConnection");
+        }
+
+        private IDbConnection Connection => new MySqlConnection(_connectionString);
+
+        public IEnumerable<Department> GetAll()
+        {
+            using var db = Connection;
+            return db.Query<Department>("SELECT * FROM department");
+        }
+
+        public Department GetById(int id)
+        {
+            using var db = Connection;
+            return db.QueryFirstOrDefault<Department>("SELECT * FROM department WHERE DepartmentId = @id", new { id });
+        }
+
+        public void Add(Department dept)
+        {
+            using var db = Connection;
+            string sql = @"INSERT INTO department (Name, ParentDepartmentId, HeadId, Description, CreatedBy, CreatedAt)
+                       VALUES (@Name, @ParentDepartmentId, @HeadId, @Description, @CreatedBy, NOW())";
+            db.Execute(sql, dept);
+        }
+
+        public void Update(Department dept)
+        {
+            using var db = Connection;
+            string sql = @"UPDATE department SET
+                       Name = @Name,
+                       ParentDepartmentId = @ParentDepartmentId,
+                       HeadId = @HeadId,
+                       Description = @Description,
+                       UpdatedBy = @UpdatedBy,
+                       UpdatedAt = NOW()
+                       WHERE DepartmentId = @DepartmentId";
+            db.Execute(sql, dept);
+        }
+
+        public void Delete(int id)
+        {
+            using var db = Connection;
+            db.Execute("DELETE FROM department WHERE DepartmentId = @id", new { id });
+        }
+    }
+
+}
