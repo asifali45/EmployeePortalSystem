d04a681f88394c63ea552fce1f160ebba2985227
asifali45 <asifali4556nagaroor@gmail.com>
2025-06-26 22:32:36 +0530

    Added Login Validation

--- EmployeePortalSystem/ViewModels/LoginViewModel.cs
+++ EmployeePortalSystem/ViewModels/LoginViewModel.cs
@@ -4,11 +4,17 @@ namespace EmployeePortalSystem.ViewModels
 {
     public class LoginViewModel
     {
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email format")]
+        public string Email { get; set; }
 
+        [Required(ErrorMessage = "Password is required")]
+        [MinLength(4, ErrorMessage = "Password must be at least 4 characters")]
+        public string Password { get; set; }
         public int EmployeeId { get; set; }
 
-        public string Name { get; set; }
-        public string RoleName { get; set; }
+        public string? Name { get; set; }
+        public string? RoleName { get; set; }
         public bool IsAdmin { get; set; }
     }
 }
