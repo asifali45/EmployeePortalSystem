22b1edc7e5c06bcff6e0933c9e34dc8e44207948
asifali45 <asifali4556nagaroor@gmail.com>
2025-06-28 23:58:37 +0530

    Added Committee Module

---
+++ EmployeePortalSystem/Controllers/CommitteeController.cs
@@ -0,0 +1,58 @@
+﻿using EmployeePortalSystem.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using EmployeePortalSystem.Models;
+
+namespace EmployeePortalSystem.Controllers
+{
+    public class CommitteeController : Controller
+    {
+        private readonly CommitteeRepository _repository;
+
+        public CommitteeController(CommitteeRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public IActionResult CommitteeDetails()
+        {
+            // Here you would typically retrieve committee details from the repository
+            var committeeList = _repository.GetAllCommittees();
+
+
+            return View("CommitteeDetails", committeeList);
+        }
+
+        [HttpGet]
+
+        public IActionResult CreateCommittee()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public IActionResult CreateCommittee(Committee committee)
+        {
+            if(ModelState.IsValid)
+            {
+                //get the logged-in user ID from session
+                int? UserId = HttpContext.Session.GetInt32("EmployeeId");
+
+                committee.CreatedAt = DateTime.Now;
+                committee.UpdatedAt = DateTime.Now;
+                committee.CreatedBy = UserId; 
+                committee.UpdatedBy = UserId;
+                
+                
+                _repository.CreateCommittee(committee);
+
+                return RedirectToAction("CommitteeDetails");
+            }
+            return View(committee);
+        }
+        
+        
+    }
+
+}
---
+++ EmployeePortalSystem/ViewModels/CommitteeViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace EmployeePortalSystem.ViewModels
+{
+    public class CommitteeViewModel
+    {
+        public int CommitteeId { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string? HeadName { get; set; } // From Employee table
+        public string? Logo { get; set; }
+        public string? Description { get; set; }
+    }
+}
