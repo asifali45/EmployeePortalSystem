7e62719c0e48004d668795bc5dcebf115b154add
asifali45 <asifali4556nagaroor@gmail.com>
2025-07-06 20:04:59 +0530

    Award Employee side

--- EmployeePortalSystem/Models/Award.cs
+++ EmployeePortalSystem/Models/Award.cs
@@ -14,5 +14,7 @@
         public DateTime CreatedAt { get; set; }
         public int UpdatedBy { get; set; }
         public DateTime UpdatedAt { get; set; }
+
+        public string RecipientPhoto { get; set; } = string.Empty;
     }
 }
